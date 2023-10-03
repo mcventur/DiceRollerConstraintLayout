d18fee8dd1620b3037d7e481cf4f4206a92b360f
mcventur <marcelinopenide@gmail.com>
2023-10-03 11:33:07 +0200

    Buenas prácticas

--- app/src/main/java/com/mpd/pmdm/dicerollerconstraintlayout/MainActivity.kt
+++ app/src/main/java/com/mpd/pmdm/dicerollerconstraintlayout/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.mpd.pmdm.dicerollerconstraintlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,17 +12,18 @@ class MainActivity : AppCompatActivity() {
 
         //Código añadido
         val rollButton: Button = findViewById(R.id.btnRoll);
-        rollButton.setOnClickListener {
-            //Tira el dado
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Función que crea un dado, lo tira, y muestra su valor en la IU
+     */
     private fun rollDice() {
         //Creamos el dado con 6 caras
         val dice = Dice(6)
+
         //Apuntamos con una variable inmutable al textView que mostrará el valor
-        val resultTextView:TextView = findViewById(R.id.tvDiceValue)
+        val resultTextView: TextView = findViewById(R.id.tvDiceValue)
         resultTextView.text = dice.roll().toString()
     }
 }
@@ -30,8 +31,8 @@ class MainActivity : AppCompatActivity() {
 /**
  * Clase que modela un dado con un número de caras configurable
  */
-class Dice(private val numSides:Int){
-    fun roll():Int{
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
